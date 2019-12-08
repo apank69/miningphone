446bd9b937f15b7adde65f6c83c10a89b42cedcf
꧁☆☬Apank_zhy☬☆꧂ <33617545+apank69@users.noreply.github.com>
2019-12-08 18:18:13 +0700

    Update Tuyul.php

--- Tuyul.php
+++ Tuyul.php
@@ -4,7 +4,7 @@ error_reporting(0);
 
 
 function call($addr) {
-        $data= "address=$addr&sponsor= http://bitcoinrain.com";
+        $data= "address=$addr&sponsor= 082319791067";
     $cok = tempnam('tmp','avo'.rand(1000000,9999999).'tmp.txt');
     $c = curl_init("https://bitcoinrain.io");
     curl_setopt($c, CURLOPT_REFERER, "https://bitcoinrain.io");
